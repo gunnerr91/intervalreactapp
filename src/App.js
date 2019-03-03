2d09bc45bbeefc63b93a9f3697818c5e1361d066
gunnerr91 <blissvortex@hotmail.com>
2019-03-03 20:38:30 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./../src/data.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notifications: [],
+      intervalId: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ notifications: data });
+    var intervalId = setInterval(
+      () => this.setState({ notifications: data }),
+      5000
+    );
+    // store intervalId in the state so it can be accessed later:
+    this.setState({ intervalId: intervalId });
+  }
+
+  componentWillUnmount() {
+    // use intervalId from the state to clear the interval
+    clearInterval(this.state.intervalId);
+  }
+
+  removeNotification = notification => {
+    var newNotifications = this.state.notifications.filter(
+      oldNotification => oldNotification !== notification
+    );
+    this.setState({ notifications: newNotifications });
+  };
+
   render() {
     return (
       <div className="App">
@@ -19,6 +50,15 @@ class App extends Component {
           >
             Learn React
           </a>
+          {this.state.notifications &&
+            this.state.notifications.map(notification => (
+              <div
+                className="notification"
+                onClick={() => this.removeNotification(notification)}
+              >
+                {notification.name}
+              </div>
+            ))}
         </header>
       </div>
     );
